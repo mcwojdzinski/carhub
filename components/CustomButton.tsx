0e30e7c0be0af81f532c5a114c9ea5febcab3b1f
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2023-06-27 11:40:45 +0200

    Initial commit with prrettier configuration + Hero component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import Image from "next/image";
+import {CustomButtonProps} from "@/types";
+
+const CustomButton = ({title, btnType, containerStyles, handleClick}: CustomButtonProps) => {
+    return (<button disabled={false} type={btnType} className={`custom-btn ${containerStyles}`} onClick={handleClick}><span className={`flex-1`}>{title}</span></button>);
+}
+
+export default CustomButton
